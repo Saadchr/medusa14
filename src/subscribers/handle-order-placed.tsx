ae908b269e86bda40c1881207b9695433b12e809
Saad Chraibi <saadchraibi85@gmail.com>
2024-03-29 14:40:39 +0000

    Refactor handleOrderPlaced function and update email sending logic

--- src/subscribers/handle-order-placed.tsx
+++ src/subscribers/handle-order-placed.tsx
@@ -3,7 +3,6 @@ import {
   type SubscriberArgs,
   OrderService,
 } from "@medusajs/medusa";
-import { relative } from "path";
 import { render } from "@react-email/render";
 import sendgrid from "@sendgrid/mail";
 import { AppleReceiptEmail } from "../emails/appleemail";
@@ -24,62 +23,33 @@ export default async function handleOrderPlaced({
   });
 
   console.log(order);
-  sendGridService.sendEmail({
-    templateId: "d-f53a4a49d08f42a2b300de41c66921e5",
-    from: "supportclient@samistore.ma",
-    to: [
-      "saadchraibi+cty2fcleflvvxrvuxm2k@boards.trello.com",
-      "saadchraibi85@gmail.com",
-    ],
-    // to: "inbox@espacedetravailuserb61fb4da28d5191afe9c6d68e6701a7c.sendboard.com",
 
-    dynamic_template_data: {
-      client: `${order.shipping_address.first_name} ${order.shipping_address.last_name}`,
-      paid_total: order.total,
-      ordernumber: order.display_id,
-      email: order.customer.email,
-      subtotal: order.subtotal,
-      total: order.total,
-      tax_total: order.tax_total,
-      shipping_total: order.shipping_total,
-      items: order.items.map((item) => ({
-        title: item.title,
-        quantity: item.quantity,
-        discounted_price: item.discount_total,
-      })),
-      shipping_address: `${order.shipping_address.address_1}, ${order.shipping_address.address_2} ${order.shipping_address.city}, ${order.shipping_address.postal_code}, ${order.shipping_address.country_code}`,
-      order_number: order.display_id,
-      phone: order.shipping_address.phone,
-      site: "https://samistore.ma",
-    },
-  });
-
-  const emailoptions = {
-    client: `${order.shipping_address.first_name} ${order.shipping_address.last_name}`,
-    created_at: order.created_at.toLocaleDateString(),
-    city: order.shipping_address.city,
-    paid_total: order.total,
-    ordernumber: order.display_id,
-    email: order.customer.email,
-    subtotal: order.subtotal,
-    total: Number(order.total).toFixed(2),
-    tax_total: Number(order.tax_total).toFixed(2),
-    shipping_total: Number(order.shipping_total).toFixed(2),
-    items: order.items.map((item) => ({
-      id: item.id,
-      thumbnail: item.thumbnail,
-      title: item.title,
-      quantity: item.quantity,
-      original_price: Number(item.unit_price).toFixed(2),
-    })),
-    shipping_address: `${order.shipping_address.address_1}, ${order.shipping_address.address_2} `,
-    order_number: order.display_id,
-    phone: order.shipping_address.phone,
-    site: "https://samistore.ma",
-  };
-
-  const emailHtml: any = render(
-    <AppleReceiptEmail emailoptions={emailoptions} />
+  const emailHtml = render(
+    <AppleReceiptEmail
+      emailoptions={{
+        client: `${order.shipping_address.first_name} ${order.shipping_address.last_name}`,
+        created_at: order.created_at.toLocaleDateString(),
+        city: order.shipping_address.city,
+        paid_total: order.total,
+        ordernumber: order.display_id,
+        email: order.customer.email,
+        subtotal: order.subtotal,
+        total: Number(order.total).toFixed(2),
+        tax_total: Number(order.tax_total).toFixed(2),
+        shipping_total: Number(order.shipping_total).toFixed(2),
+        items: order.items.map((item) => ({
+          id: item.id,
+          thumbnail: item.thumbnail,
+          title: item.title,
+          quantity: item.quantity,
+          original_price: Number(item.unit_price).toFixed(2),
+        })),
+        shipping_address: `${order.shipping_address.address_1}, ${order.shipping_address.address_2}`,
+        order_number: order.display_id,
+        phone: order.shipping_address.phone,
+        site: "https://samistore.ma",
+      }}
+    />
   );
 
   const options = {
@@ -89,7 +59,40 @@ export default async function handleOrderPlaced({
     html: emailHtml,
   };
 
-  sendgrid.send(options);
+  try {
+    await sendGridService.sendEmail({
+      templateId: "d-f53a4a49d08f42a2b300de41c66921e5",
+      from: "supportclient@samistore.ma",
+      to: [
+        "saadchraibi+cty2fcleflvvxrvuxm2k@boards.trello.com",
+        "saadchraibi85@gmail.com",
+      ],
+      dynamic_template_data: {
+        client: `${order.shipping_address.first_name} ${order.shipping_address.last_name}`,
+        paid_total: order.total,
+        ordernumber: order.display_id,
+        email: order.customer.email,
+        subtotal: order.subtotal,
+        total: order.total,
+        tax_total: order.tax_total,
+        shipping_total: order.shipping_total,
+        items: order.items.map((item) => ({
+          title: item.title,
+          quantity: item.quantity,
+          discounted_price: item.discount_total,
+        })),
+        shipping_address: `${order.shipping_address.address_1}, ${order.shipping_address.address_2} ${order.shipping_address.city}, ${order.shipping_address.postal_code}, ${order.shipping_address.country_code}`,
+        order_number: order.display_id,
+        phone: order.shipping_address.phone,
+        site: "https://samistore.ma",
+      },
+    });
+
+    await sendgrid.send(options);
+  } catch (error) {
+    console.error("Error sending email:", error);
+    // Handle the error appropriately
+  }
 }
 
 export const config: SubscriberConfig = {
