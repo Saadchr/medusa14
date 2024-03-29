50fa45c33dd7d9f8c9de6a774ec6cb50d3e39514
Saad Chraibi <saadchraibi85@gmail.com>
2024-03-29 16:46:22 +0000

    Fix SendGrid template ID and remove unused code

--- src/subscribers/order-placed.tsx
+++ src/subscribers/order-placed.tsx
@@ -59,40 +59,35 @@ export default async function handleOrderPlaced({
     html: emailHtml,
   };
 
-  try {
-    await sendGridService.sendEmail({
-      templateId: "d-f53a4a49d08f42a2b300de41c66921e5",
-      from: "supportclient@samistore.ma",
-      to: [
-        "saadchraibi+cty2fcleflvvxrvuxm2k@boards.trello.com",
-        "saadchraibi85@gmail.com",
-      ],
-      dynamic_template_data: {
-        client: `${order.shipping_address.first_name} ${order.shipping_address.last_name}`,
-        paid_total: order.total,
-        ordernumber: order.display_id,
-        email: order.customer.email,
-        subtotal: order.subtotal,
-        total: order.total,
-        tax_total: order.tax_total,
-        shipping_total: order.shipping_total,
-        items: order.items.map((item) => ({
-          title: item.title,
-          quantity: item.quantity,
-          discounted_price: item.discount_total,
-        })),
-        shipping_address: `${order.shipping_address.address_1}, ${order.shipping_address.address_2} ${order.shipping_address.city}, ${order.shipping_address.postal_code}, ${order.shipping_address.country_code}`,
-        order_number: order.display_id,
-        phone: order.shipping_address.phone,
-        site: "https://samistore.ma",
-      },
-    });
+  sendGridService.sendEmail({
+    templateId: "d-f53a4a49d08f42a2b300de41c66921e5",
+    from: "supportclient@samistore.ma",
+    to: [
+      "saadchraibi+cty2fcleflvvxrvuxm2k@boards.trello.com",
+      "saadchraibi85@gmail.com",
+    ],
+    dynamic_template_data: {
+      client: `${order.shipping_address.first_name} ${order.shipping_address.last_name}`,
+      paid_total: order.total,
+      ordernumber: order.display_id,
+      email: order.customer.email,
+      subtotal: order.subtotal,
+      total: order.total,
+      tax_total: order.tax_total,
+      shipping_total: order.shipping_total,
+      items: order.items.map((item) => ({
+        title: item.title,
+        quantity: item.quantity,
+        discounted_price: item.discount_total,
+      })),
+      shipping_address: `${order.shipping_address.address_1}, ${order.shipping_address.address_2} ${order.shipping_address.city}, ${order.shipping_address.postal_code}, ${order.shipping_address.country_code}`,
+      order_number: order.display_id,
+      phone: order.shipping_address.phone,
+      site: "https://samistore.ma",
+    },
+  });
 
-    await sendgrid.send(options);
-  } catch (error) {
-    console.error("Error sending email:", error);
-    // Handle the error appropriately
-  }
+  sendgrid.send(options);
 }
 
 export const config: SubscriberConfig = {
